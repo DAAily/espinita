9aa63c76d6a4e2821210ac06de30a5f90bd512e4
Sergio Márquez <smarquezs@gmail.com>
2022-10-10 17:27:38 -0300

    Update deprecated call.

--- lib/espinita/auditor_request.rb
+++ lib/espinita/auditor_request.rb
@@ -1,19 +1,14 @@
-module Espinita::AuditorRequest 
+module Espinita::AuditorRequest
   extend ActiveSupport::Concern
 
   included do
-    if Rails.version >= '5.1'
-      before_action :store_audited_user
-    else
-      before_filter :store_audited_user
-    end
+    before_action :store_audited_user
   end
 
   def store_audited_user
-
     # assign current_user if defined
     RequestStore.store[:audited_user] = self.send(Espinita.current_user_method) if self.respond_to?(Espinita.current_user_method, include_private = true)
-    
+
     RequestStore.store[:audited_ip]   = self.try(:request).try(:remote_ip)
   end
 end
